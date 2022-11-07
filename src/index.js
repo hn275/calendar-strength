f5dbd4a10d67dc03bb60fa51b81900f126183b3b
HalN <haln_01@proton.me>
2022-11-06 18:01:53 -0800

    setup output log

--- src/index.js
+++ src/index.js
@@ -50,6 +50,8 @@ interface TimeAvailable {
     people.push(entry);
   }
 
+  return people;
+
   let allTimes = [];
   /**
    * is there a way we could extract `allTimes` within the previous loop?
@@ -137,3 +139,76 @@ interface TimeAvailable {
     return sameDateObjects;
   }
 };
+
+const availabilites = [
+  {
+    weekOf: "2022-10-30",
+    times: [
+      {
+        startTime: "2022-10-30T09:30:00",
+        endTime: "2022-10-30T11:00:00",
+      },
+      {
+        startTime: "2022-10-31T13:30:00",
+        endTime: "2022-10-31T17:00:00",
+      },
+      {
+        startTime: "2022-11-01T08:00:00",
+        endTime: "2022-11-01T16:30:00",
+      },
+    ],
+    name: "Ben",
+    number: "125",
+  },
+  {
+    weekOf: "2022-10-30",
+    times: [
+      {
+        startTime: "2022-10-30T10:00:00",
+        endTime: "2022-10-30T12:00:00",
+      },
+      {
+        startTime: "2022-10-31T13:30:00",
+        endTime: "2022-10-31T15:00:00",
+      },
+      {
+        startTime: "2022-11-01T09:00:00",
+        endTime: "2022-11-01T16:30:00",
+      },
+    ],
+    name: "Aman",
+    number: "143",
+  },
+  {
+    weekOf: "2022-10-30",
+    times: [
+      {
+        startTime: "2022-10-30T11:30:00",
+        endTime: "2022-10-30T14:00:00",
+      },
+      {
+        startTime: "2022-10-31T15:00:00",
+        endTime: "2022-10-31T17:00:00",
+      },
+      {
+        startTime: "2022-11-01T11:00:00",
+        endTime: "2022-11-01T17:30:00",
+      },
+    ],
+    name: "Hal",
+    number: "123",
+  },
+];
+
+const data = {
+  availabilities: availabilites,
+};
+
+console.log("Table format");
+const output = splitAvailabilities(data);
+console.table(output);
+
+console.log('\n"json" format');
+for (const thing of output) {
+  console.log(thing);
+}
